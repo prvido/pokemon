653c5f2f29248aea4aad4c6226580cdcf8d8d34b
prvid <xprvidox@gmail.com>
2020-11-12 14:10:11 -0300

    Escolhe aleatóriamente entre os 100 primeiros pokemons

--- src/components/CardPanel.jsx
+++ src/components/CardPanel.jsx
@@ -4,33 +4,61 @@ import PokeCard from './PokeCard'
 
 import './CardPanel.css'
 
-const initialState = {}
+const initialState = {
+    idArray: [],
+    status: false
+}
+
+function getRandomArray (n, factor) {
+    const array = []
+    let i = 0
+    while (i < n) {
+        const aux = Math.floor(Math.random() * factor)
+        if (!array.includes(aux)) {
+            array.push(aux)
+            i++
+        }
+    }
+    return array
+}
 
 export default class CardPanel extends React.Component {
 
     state = {...initialState}
 
+    async componentDidMount() {
+        this.setState({idArray: getRandomArray(6, 100)})
+        this.setState({status: true})
+        console.log(this.state.idArray[0])
+    }
+
     render() {
-        return (
-            <div id="card-panel">
-                <div className="panelTitle">
-                    <h2>Pokémon Team</h2>
+        if (this.state.status) {
+            return (
+                <div className="card-panel">
+                    <div className="panelTitle">
+                        <h2>Pokémon Team</h2>
+                    </div>
+                    <div className="panelCards">
+                        <div className="cardRow">
+                            <PokeCard id={this.state.idArray[0]} moves={[0, 1, 2, 3]}/>
+                            <PokeCard id={this.state.idArray[1]} moves={[0, 1, 2, 3]}/>
+                        </div>
+                        <div className="cardRow">
+                            <PokeCard id={this.state.idArray[2]} moves={[0, 1, 2, 3]}/>
+                            <PokeCard id={this.state.idArray[3]} moves={[0, 1, 2, 3]}/>
+                        </div>
+                        <div className="cardRow">
+                            <PokeCard id={this.state.idArray[4]} moves={[0, 1, 2, 3]}/>
+                            <PokeCard id={this.state.idArray[5]} moves={[0, 1, 2, 3]}/>
+                        </div>
+                        </div>
                 </div>
-                <div className="panelCards">
-                    <div className="cardRow">
-                        <PokeCard id={3} moves={[0, 1, 2, 3]}/>
-                        <PokeCard id={6} moves={[0, 1, 2, 3]}/>
-                    </div>
-                    <div className="cardRow">
-                        <PokeCard id={9} moves={[0, 1, 2, 3]}/>
-                        <PokeCard id={12} moves={[0, 1, 2, 3]}/>
-                    </div>
-                    <div className="cardRow">
-                        <PokeCard id={15} moves={[0, 1, 2, 3]}/>
-                        <PokeCard id={18} moves={[0, 1, 2, 3]}/>
-                    </div>
-                    </div>
-            </div>
-        )
+            )
+        } else {
+            return (
+                <div className="card-panel">Carregando</div>
+            )
+        }
     }
 }
