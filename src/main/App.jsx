954648d78b5f8dfa2b005342b5d15fa0a991a8f3
prvid <xprvidox@gmail.com>
2020-11-13 10:06:04 -0300

    App vitou um componente funcional

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,26 +1,18 @@
 import React from 'react'
-import axios from 'axios'
-
 import CardPanel from '../components/CardPanel'
 import Header from '../template/Header'
 import Footer from '../template/Footer'
 
 import './App.css'
 
-const initialState = {
-    data: {},
-    status: 0
+const App = props => {
+    return (
+        <div id = "app">
+            <Footer/>
+            <Header/>
+            <CardPanel/>
+        </div>
+    )
 }
 
-export default class App extends React.Component {
-    state = {...initialState}
-
-    render() {
-        return (
-            <div id = "app">
-                <Footer/>
-                <Header/>
-                <CardPanel/>
-            </div>)
-    }
-}
+export default App
